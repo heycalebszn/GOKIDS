b8d9104c93eedc6f407355721c65660cbf2b5640
caleb <kalejaiyecaleb@gmail.com>
2025-01-01 18:02:50 +0100

    commit  2025 init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import { Section } from "../components/Section";
+import { Hero } from "../sections"
 
 const Home = () => {
     return(
         <Section>
-            <div></div>
+            <Hero />
         </Section>
     )
 }
